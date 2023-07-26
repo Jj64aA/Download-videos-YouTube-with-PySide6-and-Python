0ab7e34d51b59a28081fddedf3566c90479ea41e
Khamed Mohammed Taha <115487470+Jj64aA@users.noreply.github.com>
2023-07-26 06:22:01 -1100

    Update main.py delet after comment

--- main.py
+++ main.py
@@ -1460,11 +1460,6 @@ class Ui_Form(object):
         self.lineEdit.setText('')
         self.lineEdit_2.setText('')
 
-        '''
-        yt = YouTube(url)     #yt is varible
-        stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-        stream.download(filename=save_loction,on_progress_callback=handel_progras)'''
-
     def combo_box_changed(self):
         pass
         # Update the label with the selected option
